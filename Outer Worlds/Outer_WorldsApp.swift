f41c5d72b869c719f8a05dcb30ba8b3b3532dbda
Nic Dillon <dilloncnicholas@gmail.com>
2023-06-12 13:07:41 -0500

    Initial Commit

---
+++ Outer Worlds/Outer_WorldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outer_WorldsApp.swift
+//  Outer Worlds
+//
+//  Created by Nic Dillon on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Outer_WorldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
